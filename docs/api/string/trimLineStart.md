397930e1fa67350af09efc5586f9f4e0521d95c7
小恸恸 <wtt150249@gmail.com>
2023-05-23 20:47:58 +0800

    feat: 在 trimLineStart 方法中添加 removeFirstEmptyLine 和 removeLastEmptyLine 参数

--- docs/api/string/trimLineStart.md
+++ docs/api/string/trimLineStart.md
@@ -5,6 +5,9 @@
 ## params
 
 -   str (string)：要转换的字符串
+-   options(object): 配置项
+    -   removeFirstEmptyLine (boolean)：是否删除第一行的空行。默认值为 false
+    -   removeLastEmptyLine (boolean)：是否删除最后一行的空行。默认值为 false
 
 ## returns
 
--- src/string/trimLineStart.js
+++ src/string/trimLineStart.js
@@ -1,27 +1,40 @@
 /**
  * 删除行前的空格，保留缩进层级，如果是多行，那么会删除所有行的最小共有空格数
  * @param {string} str
+ * @param {object} options
+ * @param {boolean} [options.removeFirstEmptyLine=false] 是否删除第一行的空行。默认值为 false
+ * @param {boolean} [options.removeLastEmptyLine=false] 是否删除最后一行的空行。默认值为 false
  * @returns {string} 返回删除行前空格 format 后的字符串
  * @example
  * trimLineStart("  123") // "123"
  * trimLineStart("  123\n  456") // "123\n456"
  * trimLineStart("  123\n    456") // "123\n  456"
  */
-export const trimLineStart = (str) => {
-	// 正则表达式这样写是为了不匹配没有内容的行，否则会匹配到空行
-	const startSpaceAtLine = str.match(/^(?!$)[ \t]*/gm);
+export const trimLineStart = (str, options) => {
+	const { removeFirstEmptyLine = false, removeLastEmptyLine = false } =
+		options || {};
 
-	if (!startSpaceAtLine) {
+	if (removeFirstEmptyLine) {
+		str = str.replace(/^\s*\n/, "");
+	}
+	if (removeLastEmptyLine) {
+		str = str.replace(/\n\s*$/, "");
+	}
+
+	// 正则表达式这样写是为了不匹配没有内容的行，否则会匹配到空行
+	const leadingSpacesAtLine = str.match(/^(?!$)[ \t]*/gm);
+
+	if (!leadingSpacesAtLine) {
 		return str;
 	}
 
 	// 如果只有一行，那么直接使用 trimStart 返回就行，节约性能
-	if (startSpaceAtLine.length === 1) {
+	if (leadingSpacesAtLine.length === 1) {
 		return str.trimStart();
 	}
 
 	// 如果有多行，那么就遍历所有行，获取共同的最小的空格数，然后每行删除最小空格数的空格
-	const minSapceNum = startSpaceAtLine.reduce((min, line) => {
+	const minSapceNum = leadingSpacesAtLine.reduce((min, line) => {
 		return Math.min(
 			typeof min === "number" ? min : min.length,
 			line.length
